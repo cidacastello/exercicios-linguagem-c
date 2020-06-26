735627abff35f13fab1faa88d3e3234d5072e6e6
Cida Castello <56411540+cidacastello@users.noreply.github.com>
2020-06-26 15:57:39 -0300

    Create Ex-04-MUV.c

---
+++ exercicios-codigo-fonte/Ex-04-MUV/Ex-04-MUV.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	//declaração e inicialização das variáveis
+	float velocidadeInicial=10, aceleracao=3, tempo, velocidade;
+		
+	printf("***** M.V.U. *****\n\n");
+	
+	
+	printf("Informe o tempo em segundos: ");
+	
+	//entrada de dados
+	scanf("%f", &tempo);
+	
+	//processamento
+	velocidade = velocidadeInicial + 3 * tempo;
+	
+	//saída de dados
+	printf("\nA velocidade em m/s e: %f\n\n", velocidade);
+	getch();	
+	return 0;
+}
